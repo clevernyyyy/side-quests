6f46c7782f8ef07755cf087bd84cf2341a618a83
Jeris Rue <jeris.rue@gmail.com>
2025-03-29 17:09:43 -0500

    add user back to django admin

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Score, Badge
+from .models import Score, Badge, CustomUser
 
 class BadgeAdmin(admin.ModelAdmin):
     list_display = ["machine_id", "initials", "added", "updated"]
 
 
+admin.site.register(CustomUser)
 admin.site.register(Score)
 admin.site.register(Badge, BadgeAdmin)
