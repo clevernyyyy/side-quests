4698ed48f9efe3026a4c3f759d04a6c21acabf0d
Jeris Rue <jeris.rue@gmail.com>
2025-03-29 11:55:26 -0500

    support for badge updates

--- scoreboard/tasks.py
+++ scoreboard/tasks.py
@@ -3,11 +3,10 @@ from celery import shared_task
 from django.conf import settings
 from django.db.models import Sum
 from django.db.models.functions import Coalesce
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from .models import Badge
 
 import requests
-import json
 
 
 BADGE_LEADERBOARD_BASE_URL = settings.BADGE_LEADERBOARD_BASE_URL
@@ -15,15 +14,34 @@ TOKEN = settings.BADGE_LEADERBOARD_API_TOKEN
 headers = {"X-API-Key": TOKEN}
 
 def save_model_from_json(json_data):
+
     try:
-         model_instance = Badge(**json_data)
-         model_instance.full_clean()
-         model_instance.save()
-         return model_instance
+        badge = Badge.objects.get(machine_id=json_data['machine_id'])
+
+        # badge exists, update all attributes except machine id
+        for attr, value in json_data.items():
+            if attr != 'machine_id':
+                setattr(badge, attr, value)
+        
+        badge.full_clean()
+        badge.save()
+        return badge
     except TypeError as e:
         raise ValueError(f"Incorrect data format: {e}")
     except ValidationError as e:
-        raise ValueError(f"Validation error: {e}")
+        raise ValueError(f"Validation error: {e}")   
+    except ObjectDoesNotExist:
+
+        try:
+            # badge doesn't exist, add new instance
+            model_instance = Badge(**json_data)
+            model_instance.full_clean()
+            model_instance.save()
+            return model_instance
+        except TypeError as e:
+            raise ValueError(f"Incorrect data format: {e}")
+        except ValidationError as e:
+            raise ValueError(f"Validation error: {e}")
 
 
 @shared_task
